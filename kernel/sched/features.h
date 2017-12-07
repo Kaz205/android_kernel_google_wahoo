10944f24d4bcfa57876f0c6d183229f20903f147
Ionela Voinescu <ionela.voinescu@arm.com>
2017-12-07 19:43:46 +0000

    sched/fair: introduce minimum capacity capping sched feature
    We have the ability to track minimum capacity forced onto a CPU by
    userspace or external actors. This is provided though a minimum
    frequency scale factor exposed by arch_scale_min_freq_capacity.
    
    The use of this information is enabled through the MIN_CAPACITY_CAPPING
    feature. If not enabled, the minimum frequency scale factor will
    remain 0 and it will not impact energy estimation or scheduling
    decisions.
    
    Change-Id: Ibc61f2bf4fddf186695b72b262e602a6e8bfde37
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -80,3 +80,11 @@
 #else
 #define SCHED_FEAT_ENERGY_AWARE 0
 #endif
+
+/*
+ * Minimum capacity capping. Keep track of minimum capacity factor when
+ * minimum frequency available to a policy is modified.
+ * If enabled, this can be used to inform the scheduler about capacity
+ * restrictions.
+ */
+SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
